61bd5ec4e40a5831b68c2eb2b551cce95970ac41
MukunaChristian <christianmbuyimukuna@gmail.com>
2024-06-18 14:25:16 +0200

    added change

--- script/top.js
+++ script/top.js
@@ -1,37 +1,37 @@
-function toggleNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+// function toggleNav() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "topnav";
+//     }
+// }
 
 
-// animation.js
+// // animation.js
 
-document.addEventListener('DOMContentLoaded', () => {
-    const fadeInElements = document.querySelectorAll('.fade-in');
-    const fadeInLeftElements = document.querySelectorAll('.fade-in-left');
-    const fadeInRightElements = document.querySelectorAll('.fade-in-right');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const fadeInElements = document.querySelectorAll('.fade-in');
+//     const fadeInLeftElements = document.querySelectorAll('.fade-in-left');
+//     const fadeInRightElements = document.querySelectorAll('.fade-in-right');
   
-    const options = {
-      threshold: 0.1
-    };
+//     const options = {
+//       threshold: 0.1
+//     };
   
-    const handleIntersect = (entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('visible');
-          observer.unobserve(entry.target);
-        }
-      });
-    };
+//     const handleIntersect = (entries, observer) => {
+//       entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//           entry.target.classList.add('visible');
+//           observer.unobserve(entry.target);
+//         }
+//       });
+//     };
   
-    const observer = new IntersectionObserver(handleIntersect, options);
+//     const observer = new IntersectionObserver(handleIntersect, options);
   
-    fadeInElements.forEach(element => observer.observe(element));
-    fadeInLeftElements.forEach(element => observer.observe(element));
-    fadeInRightElements.forEach(element => observer.observe(element));
-  });
+//     fadeInElements.forEach(element => observer.observe(element));
+//     fadeInLeftElements.forEach(element => observer.observe(element));
+//     fadeInRightElements.forEach(element => observer.observe(element));
+//   });
   
