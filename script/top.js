a2d640d978cf629247fb9752bb602a42349e5a59
MukunaChristian <christianmbuyimukuna@gmail.com>
2024-06-18 13:09:39 +0200

    addded change

--- script/top.js
+++ script/top.js
@@ -6,3 +6,32 @@ function toggleNav() {
         x.className = "topnav";
     }
 }
+
+
+// animation.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const fadeInElements = document.querySelectorAll('.fade-in');
+    const fadeInLeftElements = document.querySelectorAll('.fade-in-left');
+    const fadeInRightElements = document.querySelectorAll('.fade-in-right');
+  
+    const options = {
+      threshold: 0.1
+    };
+  
+    const handleIntersect = (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+  
+    const observer = new IntersectionObserver(handleIntersect, options);
+  
+    fadeInElements.forEach(element => observer.observe(element));
+    fadeInLeftElements.forEach(element => observer.observe(element));
+    fadeInRightElements.forEach(element => observer.observe(element));
+  });
+  
