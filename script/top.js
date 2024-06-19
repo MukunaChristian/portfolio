e6685aa783b80a5825e001acf5e94ecec41fc2fa
MukunaChristian <christianmbuyimukuna@gmail.com>
2024-06-19 18:28:20 +0200

    added change

--- script/top.js
+++ script/top.js
@@ -8,32 +8,7 @@ function toggleNav() {
 }
 
 
-// animation.js
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const fadeInElements = document.querySelectorAll('.fade-in');
-//     const fadeInLeftElements = document.querySelectorAll('.fade-in-left');
-//     const fadeInRightElements = document.querySelectorAll('.fade-in-right');
-  
-//     const options = {
-//       threshold: 0.1
-//     };
-  
-//     const handleIntersect = (entries, observer) => {
-//       entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//           entry.target.classList.add('visible');
-//           observer.unobserve(entry.target);
-//         }
-//       });
-//     };
-  
-//     const observer = new IntersectionObserver(handleIntersect, options);
-  
-//     fadeInElements.forEach(element => observer.observe(element));
-//     fadeInLeftElements.forEach(element => observer.observe(element));
-//     fadeInRightElements.forEach(element => observer.observe(element));
-//   });
   
 document.addEventListener("DOMContentLoaded", function() {
   const observerOptions = {
